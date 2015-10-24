60ffb6ae95edb45047a0a96231ebcfb4f4d93db0
Matheus Sampaio <msampaio.mail@gmail.com>
2015-10-24 13:50:32 -0300

    "teste commit eclipse"

--- cplusplus/comunicaosensores/include/Comunicacao.h
+++ cplusplus/comunicaosensores/include/Comunicacao.h
@@ -46,7 +46,7 @@ private:
 public:
 	Comunicacao(char* porta);
 
-	//iniciar a comunicação com a porta Serial -- retorna 0 se der tudo certo, retorna #! se ocorrer problema
+	//iniciar a comunicacao com a porta Serial -- retorna 0 se der tudo certo, retorna #! se ocorrer problema
 	int iniciar();
 
 	//realizar a leitura de um buffer a partir de uma porta Serial
