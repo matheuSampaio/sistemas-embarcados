52447cdc796eb14a60624afcff5c87623e9f6c45
huggobrito <huggobrito@gmail.com>
2015-11-05 18:02:09 -0300

    Correção no Executor

--- java/JavaApp/src/br/edu/ifba/bsi/embarcados/regua/Executor.java
+++ java/JavaApp/src/br/edu/ifba/bsi/embarcados/regua/Executor.java
@@ -1,24 +1,25 @@
 package br.edu.ifba.bsi.embarcados.regua;
 
+import br.edu.ifba.bsi.embarcados.regua.asincexec.AsincExec;
 import br.edu.ifba.bsi.embarcados.regua.conector.FabricaConectores;
 import br.edu.ifba.bsi.embarcados.regua.conector.IComunicacaoSensores;
 
 public class Executor {
 
 	public static void main(String[] args) throws InterruptedException {
-		IComunicacaoSensores conector = FabricaConectores.getConector();
-		if(conector.iniciar("COM6")==0){//sucesso
-			while(true){
-				conector.ler();
-//				System.out.println("AcelX = "+conector.getAcelX());
-//				System.out.println("AcelY = "+conector.getAcelY());
-//				System.out.println("AcelZ = "+conector.getAcelZ());
-				
-				System.out.println("Angulo YZ:"+ (int) conector.getAnguloYZ());
-				System.out.println("Angulo XZ:"+ (int) conector.getAnguloXZ());
-				
-				Thread.sleep(100);
-			}
+		ListenerAcelerometro listener = new ListenerAcelerometro();
+		AsincExec asinc = new AsincExec("COM6");
+		asinc.addListener(listener);
+
+		Thread t = new Thread(asinc);
+		t.start();
+
+		for (int i = 0; i < 10; i++) {
+			Thread.sleep(1000);
 		}
+		// parar a thread
+		asinc.setContinuar(false);
+		// ter certeza que parou
+		t.join();
 	}
 }
